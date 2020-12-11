19888e27a5b32b51cbe1229c599c32e4dc07bcbd
Nikolai Vasiliev <63501861+krankenmorder@users.noreply.github.com>
2020-12-11 21:05:33 +0500

    version 1.0
    Создание классов круга, квадрата, треугольника и отрезка. Создание класса-хранилища.

--- LabOOP_6/Form1.cs
+++ LabOOP_6/Form1.cs
@@ -17,6 +17,99 @@ namespace LabOOP_6
             InitializeComponent();
         }
 
-        
+        class Ellipse
+        {
+            private string name = "Ellipse";
+            public int x, y;
+            public Ellipse()
+            {
+                x = 0;
+                y = 0;
+                name = "Ellipse";
+            }
+            public Ellipse(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+            ~Ellipse() 
+            {
+
+            }
+        }
+
+        class Rectangle
+        {
+            private string name = "Rectangle";
+            public int x, y;
+            public Rectangle()
+            {
+                x = 0;
+                y = 0;
+                name = "Rectangle";
+            }
+            public Rectangle(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+            ~Rectangle()
+            {
+
+            }
+        }
+
+        class Triangle
+        {
+            private string name = "Triangle";
+            public int x, y, z;
+            public Triangle()
+            {
+                x = 0;
+                y = 0;
+                z = 0;
+                name = "Triangle";
+            }
+            public Triangle(int x, int y, int z)
+            {
+                this.x = x;
+                this.y = y;
+                this.z = z;
+            }
+            ~Triangle()
+            {
+
+            }
+        }
+
+
+        class Line
+        {
+            private string name = "Line";
+            public int x, y;
+            public Line()
+            {
+                x = 0;
+                y = 0;
+                name = "Line";
+            }
+            public Line(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+            ~Line()
+            {
+
+            }
+        }
+
+        class Storage
+        {
+            public Ellipse[] ellipses;
+            public Rectangle[] rectangles;
+            public Triangle[] triangles;
+            public Line[] lines;
+        }
     }
 }
