adf6b9632b4489474887941f273d40ddea940358
Nikolai Vasiliev <63501861+krankenmorder@users.noreply.github.com>
2020-12-17 21:25:10 +0500

    version 1.1
    Добавление возможности рисования кругов и квадратов на панели разных цветов с заливкой.

--- LabOOP_6/Form1.Designer.cs
+++ LabOOP_6/Form1.Designer.cs
@@ -29,32 +29,204 @@ namespace LabOOP_6
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.panelPaint = new System.Windows.Forms.Panel();
+            this.groupBoxChoose = new System.Windows.Forms.GroupBox();
+            this.radioButtonLine = new System.Windows.Forms.RadioButton();
+            this.radioButtonTriangle = new System.Windows.Forms.RadioButton();
+            this.radioButtonRectangle = new System.Windows.Forms.RadioButton();
+            this.radioButtonEllipse = new System.Windows.Forms.RadioButton();
+            this.groupBoxColor = new System.Windows.Forms.GroupBox();
+            this.radioButtonGreen = new System.Windows.Forms.RadioButton();
+            this.radioButtonRed = new System.Windows.Forms.RadioButton();
+            this.radioButtonOrange = new System.Windows.Forms.RadioButton();
+            this.radioButtonYellow = new System.Windows.Forms.RadioButton();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.labelСoords = new System.Windows.Forms.Label();
+            this.groupBoxChoose.SuspendLayout();
+            this.groupBoxColor.SuspendLayout();
             this.SuspendLayout();
             // 
-            // panel1
+            // panelPaint
             // 
-            this.panel1.BackColor = System.Drawing.Color.White;
-            this.panel1.Location = new System.Drawing.Point(12, 12);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(600, 600);
-            this.panel1.TabIndex = 0;
+            this.panelPaint.BackColor = System.Drawing.Color.White;
+            this.panelPaint.Location = new System.Drawing.Point(12, 12);
+            this.panelPaint.Name = "panelPaint";
+            this.panelPaint.Size = new System.Drawing.Size(600, 600);
+            this.panelPaint.TabIndex = 0;
+            this.panelPaint.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panelPaint_MouseClick);
+            // 
+            // groupBoxChoose
+            // 
+            this.groupBoxChoose.Controls.Add(this.radioButtonLine);
+            this.groupBoxChoose.Controls.Add(this.radioButtonTriangle);
+            this.groupBoxChoose.Controls.Add(this.radioButtonRectangle);
+            this.groupBoxChoose.Controls.Add(this.radioButtonEllipse);
+            this.groupBoxChoose.Location = new System.Drawing.Point(618, 12);
+            this.groupBoxChoose.Name = "groupBoxChoose";
+            this.groupBoxChoose.Size = new System.Drawing.Size(252, 134);
+            this.groupBoxChoose.TabIndex = 1;
+            this.groupBoxChoose.TabStop = false;
+            this.groupBoxChoose.Text = "Выбор элемента рисования:";
+            // 
+            // radioButtonLine
+            // 
+            this.radioButtonLine.AutoSize = true;
+            this.radioButtonLine.Location = new System.Drawing.Point(7, 106);
+            this.radioButtonLine.Name = "radioButtonLine";
+            this.radioButtonLine.Size = new System.Drawing.Size(85, 21);
+            this.radioButtonLine.TabIndex = 3;
+            this.radioButtonLine.TabStop = true;
+            this.radioButtonLine.Text = "Отрезок";
+            this.radioButtonLine.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonTriangle
+            // 
+            this.radioButtonTriangle.AutoSize = true;
+            this.radioButtonTriangle.Location = new System.Drawing.Point(7, 78);
+            this.radioButtonTriangle.Name = "radioButtonTriangle";
+            this.radioButtonTriangle.Size = new System.Drawing.Size(112, 21);
+            this.radioButtonTriangle.TabIndex = 2;
+            this.radioButtonTriangle.TabStop = true;
+            this.radioButtonTriangle.Text = "Треугольник";
+            this.radioButtonTriangle.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonRectangle
+            // 
+            this.radioButtonRectangle.AutoSize = true;
+            this.radioButtonRectangle.Location = new System.Drawing.Point(7, 50);
+            this.radioButtonRectangle.Name = "radioButtonRectangle";
+            this.radioButtonRectangle.Size = new System.Drawing.Size(84, 21);
+            this.radioButtonRectangle.TabIndex = 1;
+            this.radioButtonRectangle.TabStop = true;
+            this.radioButtonRectangle.Text = "Квадрат";
+            this.radioButtonRectangle.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonEllipse
+            // 
+            this.radioButtonEllipse.AutoSize = true;
+            this.radioButtonEllipse.Location = new System.Drawing.Point(7, 22);
+            this.radioButtonEllipse.Name = "radioButtonEllipse";
+            this.radioButtonEllipse.Size = new System.Drawing.Size(58, 21);
+            this.radioButtonEllipse.TabIndex = 0;
+            this.radioButtonEllipse.TabStop = true;
+            this.radioButtonEllipse.Text = "Круг";
+            this.radioButtonEllipse.UseVisualStyleBackColor = true;
+            // 
+            // groupBoxColor
+            // 
+            this.groupBoxColor.Controls.Add(this.radioButtonGreen);
+            this.groupBoxColor.Controls.Add(this.radioButtonRed);
+            this.groupBoxColor.Controls.Add(this.radioButtonOrange);
+            this.groupBoxColor.Controls.Add(this.radioButtonYellow);
+            this.groupBoxColor.Location = new System.Drawing.Point(619, 153);
+            this.groupBoxColor.Name = "groupBoxColor";
+            this.groupBoxColor.Size = new System.Drawing.Size(251, 131);
+            this.groupBoxColor.TabIndex = 2;
+            this.groupBoxColor.TabStop = false;
+            this.groupBoxColor.Text = "Цвет:";
+            // 
+            // radioButtonGreen
+            // 
+            this.radioButtonGreen.AutoSize = true;
+            this.radioButtonGreen.BackColor = System.Drawing.Color.Lime;
+            this.radioButtonGreen.Location = new System.Drawing.Point(7, 103);
+            this.radioButtonGreen.Name = "radioButtonGreen";
+            this.radioButtonGreen.Size = new System.Drawing.Size(88, 21);
+            this.radioButtonGreen.TabIndex = 3;
+            this.radioButtonGreen.TabStop = true;
+            this.radioButtonGreen.Text = "Зелёный";
+            this.radioButtonGreen.UseVisualStyleBackColor = false;
+            // 
+            // radioButtonRed
+            // 
+            this.radioButtonRed.AutoSize = true;
+            this.radioButtonRed.BackColor = System.Drawing.Color.Red;
+            this.radioButtonRed.Location = new System.Drawing.Point(7, 22);
+            this.radioButtonRed.Name = "radioButtonRed";
+            this.radioButtonRed.Size = new System.Drawing.Size(87, 21);
+            this.radioButtonRed.TabIndex = 0;
+            this.radioButtonRed.TabStop = true;
+            this.radioButtonRed.Text = "Красный";
+            this.radioButtonRed.UseVisualStyleBackColor = false;
+            // 
+            // radioButtonOrange
+            // 
+            this.radioButtonOrange.AutoSize = true;
+            this.radioButtonOrange.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.radioButtonOrange.Location = new System.Drawing.Point(7, 49);
+            this.radioButtonOrange.Name = "radioButtonOrange";
+            this.radioButtonOrange.Size = new System.Drawing.Size(106, 21);
+            this.radioButtonOrange.TabIndex = 1;
+            this.radioButtonOrange.TabStop = true;
+            this.radioButtonOrange.Text = "Оранжевый";
+            this.radioButtonOrange.UseVisualStyleBackColor = false;
+            // 
+            // radioButtonYellow
+            // 
+            this.radioButtonYellow.AutoSize = true;
+            this.radioButtonYellow.BackColor = System.Drawing.Color.Yellow;
+            this.radioButtonYellow.Location = new System.Drawing.Point(7, 76);
+            this.radioButtonYellow.Name = "radioButtonYellow";
+            this.radioButtonYellow.Size = new System.Drawing.Size(83, 21);
+            this.radioButtonYellow.TabIndex = 2;
+            this.radioButtonYellow.TabStop = true;
+            this.radioButtonYellow.Text = "Жёлтый";
+            this.radioButtonYellow.UseVisualStyleBackColor = false;
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(625, 531);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(244, 56);
+            this.buttonClear.TabIndex = 3;
+            this.buttonClear.Text = "Очистить полотно";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            // 
+            // labelСoords
+            // 
+            this.labelСoords.AutoSize = true;
+            this.labelСoords.Location = new System.Drawing.Point(623, 595);
+            this.labelСoords.Name = "labelСoords";
+            this.labelСoords.Size = new System.Drawing.Size(136, 17);
+            this.labelСoords.TabIndex = 4;
+            this.labelСoords.Text = "Координаты мыши:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(882, 628);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.labelСoords);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.groupBoxColor);
+            this.Controls.Add(this.groupBoxChoose);
+            this.Controls.Add(this.panelPaint);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBoxChoose.ResumeLayout(false);
+            this.groupBoxChoose.PerformLayout();
+            this.groupBoxColor.ResumeLayout(false);
+            this.groupBoxColor.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panelPaint;
+        private System.Windows.Forms.GroupBox groupBoxChoose;
+        private System.Windows.Forms.RadioButton radioButtonLine;
+        private System.Windows.Forms.RadioButton radioButtonTriangle;
+        private System.Windows.Forms.RadioButton radioButtonRectangle;
+        private System.Windows.Forms.RadioButton radioButtonEllipse;
+        private System.Windows.Forms.GroupBox groupBoxColor;
+        private System.Windows.Forms.RadioButton radioButtonGreen;
+        private System.Windows.Forms.RadioButton radioButtonRed;
+        private System.Windows.Forms.RadioButton radioButtonOrange;
+        private System.Windows.Forms.RadioButton radioButtonYellow;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Label labelСoords;
     }
 }
 
--- LabOOP_6/Form1.cs
+++ LabOOP_6/Form1.cs
@@ -17,22 +17,24 @@ namespace LabOOP_6
             InitializeComponent();
         }
 
+        int color = 0;
+        static int razmer = 10;
+        Storage storage = new Storage(razmer);
+
         class Ellipse
         {
-            private string name = "Ellipse";
             public int x, y;
             public Ellipse()
             {
                 x = 0;
                 y = 0;
-                name = "Ellipse";
             }
             public Ellipse(int x, int y)
             {
                 this.x = x;
                 this.y = y;
             }
-            ~Ellipse() 
+            ~Ellipse()
             {
 
             }
@@ -40,13 +42,11 @@ namespace LabOOP_6
 
         class Rectangle
         {
-            private string name = "Rectangle";
             public int x, y;
             public Rectangle()
             {
                 x = 0;
                 y = 0;
-                name = "Rectangle";
             }
             public Rectangle(int x, int y)
             {
@@ -61,14 +61,12 @@ namespace LabOOP_6
 
         class Triangle
         {
-            private string name = "Triangle";
             public int x, y, z;
             public Triangle()
             {
                 x = 0;
                 y = 0;
                 z = 0;
-                name = "Triangle";
             }
             public Triangle(int x, int y, int z)
             {
@@ -85,13 +83,11 @@ namespace LabOOP_6
 
         class Line
         {
-            private string name = "Line";
             public int x, y;
             public Line()
             {
                 x = 0;
                 y = 0;
-                name = "Line";
             }
             public Line(int x, int y)
             {
@@ -110,6 +106,109 @@ namespace LabOOP_6
             public Rectangle[] rectangles;
             public Triangle[] triangles;
             public Line[] lines;
+
+            public Storage(int count)
+            {
+                ellipses = new Ellipse[count];
+                rectangles = new Rectangle[count];
+                triangles = new Triangle[count];
+                lines = new Line[count];
+
+                for (int i = 0; i < count; i++)
+                {
+                    ellipses[i] = null;
+                    rectangles[i] = null;
+                    triangles[i] = null;
+                    lines[i] = null;
+
+                }
+            }
+
+            ~Storage()
+            {
+
+            }
+        }
+
+        public void chooseColor()
+        {
+            if (radioButtonRed.Checked == true) //красный
+            {
+                color = 1;
+            }
+            if (radioButtonOrange.Checked == true) //оранжевый
+            {
+                color = 2;
+            }
+            if (radioButtonYellow.Checked == true) //жёлтый
+            {
+                color = 3;
+            }
+            if (radioButtonGreen.Checked == true) //зелёный
+            {
+                color = 4;
+            }
+        }
+
+        public void createEllipse() { 
+        }
+
+        private void panelPaint_MouseClick(object sender, MouseEventArgs e)
+        {
+            Pen red = new Pen(Color.Red, 5);
+            SolidBrush fillRed = new SolidBrush(Color.Red);
+            Pen orange = new Pen(Color.Orange, 5);
+            SolidBrush fillOrange = new SolidBrush(Color.Orange);
+            Pen yellow = new Pen(Color.Yellow, 5);
+            SolidBrush fillYellow = new SolidBrush(Color.Yellow);
+            Pen green = new Pen(Color.Lime, 5);
+            SolidBrush fillGreen = new SolidBrush(Color.Lime);
+            if (radioButtonEllipse.Checked == true)
+            {
+                Ellipse krug = new Ellipse(e.X - 25, e.Y - 25);
+                RectangleF ellipse_rectangle = new RectangleF(e.X - 25, e.Y - 25, 50, 50);
+                chooseColor();
+                switch (color)
+                {
+                    case 1:
+                        panelPaint.CreateGraphics().DrawEllipse(red, ellipse_rectangle);
+                        panelPaint.CreateGraphics().FillEllipse(fillRed, ellipse_rectangle);
+                        break;
+                    case 2:
+                        panelPaint.CreateGraphics().DrawEllipse(orange, ellipse_rectangle);
+                        panelPaint.CreateGraphics().FillEllipse(fillOrange, ellipse_rectangle);
+                        break;
+                    case 3:
+                        panelPaint.CreateGraphics().DrawEllipse(yellow, ellipse_rectangle);
+                        panelPaint.CreateGraphics().FillEllipse(fillYellow, ellipse_rectangle);
+                        break;
+                    case 4:
+                        panelPaint.CreateGraphics().DrawEllipse(green, ellipse_rectangle);
+                        panelPaint.CreateGraphics().FillEllipse(fillGreen, ellipse_rectangle);
+                        break;
+                }
+            }
+            if (radioButtonRectangle.Checked == true)
+            {
+                Rectangle kvadrat = new Rectangle(e.X - 25, e.Y - 25);
+                //Rectangle rectangle = new Rectangle(e.X - 25, e.Y - 25, 50, 50);
+                chooseColor();
+                switch (color)
+                {
+                    case 1:
+                        panelPaint.CreateGraphics().DrawRectangle(red, e.X - 25, e.Y - 25, 50, 50);
+                        break;
+                    case 2:
+                        panelPaint.CreateGraphics().DrawRectangle(orange, e.X - 25, e.Y - 25, 50, 50);
+                        break;
+                    case 3:
+                        panelPaint.CreateGraphics().DrawRectangle(yellow, e.X - 25, e.Y - 25, 50, 50);
+                        break;
+                    case 4:
+                        panelPaint.CreateGraphics().DrawRectangle(green, e.X - 25, e.Y - 25, 50, 50);
+                        break;
+                }
+            }
         }
     }
 }
